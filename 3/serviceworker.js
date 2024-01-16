26f272e4de98fd87ee69a4490197423c964ce3f3
rpi <95402257+rpi-esp32@users.noreply.github.com>
2024-01-16 22:40:00 +0800

    service worker

---
+++ 3/serviceworker.js
@@ -0,0 +1,27 @@
+var staticCacheName = "pwa";
+
+const assets = [
+  "/",
+  "/index.html",
+  "images/favicon.ico",
+  "images/icon-192x192.png",
+  "images/icon-512x512.png",
+];
+
+self.addEventListener("install", function (e) {
+  e.waitUntil(
+    caches.open(staticCacheName).then(function (cache) {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener("fetch", function (event) {
+  console.log(event.request.url);
+
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
+    })
+  );
+});
