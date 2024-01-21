eb9737a441079a76e917f645bc5e3b4b85a95fdc
rpi <95402257+rpi-esp32@users.noreply.github.com>
2024-01-21 21:53:26 +0800

    w

--- script.js
+++ script.js
@@ -74,7 +74,7 @@ function success(position) {
   b.textContent = lon;
   c.textContent = acc;
   d.textContent = time;
-  document.querySelector(#content>button.active).classList.remove("active");
+  document.querySelector("#content button.active").classList.remove("active");
 }
 
 function failed() {
