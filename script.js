a88b1c76d7dfe3dc7c5a5770e0c94be15f6dd207
rpi <95402257+rpi-esp32@users.noreply.github.com>
2024-01-21 20:34:38 +0800

    a

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*  ****** NAVIGATION ********* */
 function go(event) {
   document.getElementById("goto01").classList.remove("active");
   document.getElementById("goto02").classList.remove("active");
@@ -14,10 +15,7 @@ function go(event) {
   page.classList.add("show");
 }
 
-/*
-ORIENTATION **************
-*/
-
+/*  *********** ORIENTATION ***************/
 let a = document.getElementById("value31");
 let b = document.getElementById("value32");
 let c = document.getElementById("value33");
@@ -29,7 +27,7 @@ let h = document.getElementById("value38");
 
 if ("DeviceOrientationEvent" in window) {
   window.addEventListener("deviceorientation", handler, false);
-  f.textContent = "event registered.";
+  // f.textContent = "event registered.";
 } else {
   a.textContent = "API not supported ...";
 }
@@ -40,7 +38,7 @@ function handler(event) {
   let FB = event.beta;
   let dir = event.alpha;
   let abs = event.absolute;
-  let webkit = event.webkitCompassHeading??"failed experiment";
+  let webkit = event.webkitCompassHeading??"failed compass";
 
   c.textContent = LR;
   d.textContent = FB;
@@ -53,18 +51,21 @@ function handler(event) {
  GPS ****
 *********
 ***/
-navigator.permissions.query({ name: "geolocation" }).then((result) => {
-  if (result.state === "denied") {
+navigator.permissions.query({ name: "geolocation" }).then((permit) => {
+  if (permit.state === "denied") {
     console.log("permission denied");
     return;
   } else {
-    navigator.geolocation.getCurrentPosition(result, failed, {enableHighAccuracy: true,});
+    navigator.geolocation.getCurrentPosition(success, failed, {enableHighAccuracy: true,});
   }
 });
 
-function result(position) {
+function success(position) {
   let lat = position.coords.latitude;
   let lon = position.coords.longitude;
+  let alt = position.coords.altitude;
+  let acc = position.coords.accuracy;
+  let time = position.timestamp;
   console.log(lat + "   " + lon);
   a.textContent = lat;
   b.textContent = lon;
